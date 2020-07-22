24e0c6fe12d7ccc5001481ea0d07b1de0b15bb8f
Andrew Roberts <clownfragment@gmail.com>
2020-07-22 09:21:41 -0600

    add july 21 lect

---
+++ python/2020_07_21_python_basics.py
@@ -0,0 +1,167 @@
+
+# what do you need to know to learn a new programming language
+
+    # technical:
+        # data types and type operations
+        # functions
+        # iteration
+        # conditions # control flow
+
+    # workflow:
+        # debugging tools and techniques
+            # print(), type(), dir(), help(), id()
+        # learn to love and read errors
+        # MVP
+        # testing / proofing
+
+
+
+
+
+
+
+###############################################################
+
+# python basics
+    # introduction to data types
+        # int
+        # float
+            # has a fractional value 5.3
+        # bool
+            # True, False
+# print( 5 > 5 )
+
+        # print()
+        # type()
+# print( type(5 > 7) )
+
+        # data type is inferred not explicitly stated (duck typing)
+
+    # simple type operations
+        # +
+# print( 4 + 5 )
+# print( 4.3 + 4.7 )
+# print( 4 + 5 )
+        # -
+# print( 45.3 - 5 )
+        # *
+# print( 4 * 5 )
+
+        # /
+# print( 80 / 11 )
+        # //
+# print( 80.9 // 11.4 )
+        # %
+# print( 20 % 6 )
+            # generally avoid negative numbers # slide 12
+
+        # a**n
+# print( 2**8 )
+        # a**0.n # the nth root of a
+# n = 2
+# print( 16**(1/n) )
+
+        # BREAKOUT slide 7, 13 - 17
+
+        # casting # ( converting ) one type to another
+
+# n = 3**3.9/7
+# print(  str( n )  )
+
+
+    # varriables and the assignment operator
+        # assaignment
+n = 4
+        # reassaignment
+# n = "panda bears"
+        # how to modify according to current value
+            # incrementation
+# n = n + 1
+# n %= 3
+# print(n)
+
+# scalar types
+    # overview # slide 20
+    # int
+    # float
+    # bool (boolean)
+    # str (string)
+# print( 2 + "78" )
+# print( "78" + "2" )
+# print( "hello" + " world" )
+
+    # None
+# def add_1(n):
+#     return n + 1
+
+# print( "this is line 91:", add_1(8) )
+    # BREAKOUT slide 26
+# print( bool("False") ) # True
+
+# declaring variables
+    # variable naming syntax
+    # storing / reusing data
+    # declaring variables
+    # good naming # snake case # take the time
+# my_variable_name = ""
+
+# boolean operators (logical operators)
+    # >
+# print( 5 > 99 )
+    # <
+# print( 5 < 99 )
+    # >=
+# a = 5
+# b = 5
+# print( a >= b )
+    # <=
+    # ==
+# print( 5 == 5 )
+    # !=
+# print( 5 != 6 )
+
+# print( a == b )
+
+    # not
+# print( not 5 > 3 )
+    # and
+# print( 5 > 3 and 5 > 7 )
+# print( True and True )
+# print( True and False )
+# print( False and True )
+# print( False and False )
+    # or
+# print( True or True )
+# print( True or False )
+# print( False or True )
+# print( False or False )
+    # ^ # xor
+# print( True ^ True )
+# print( True ^ False )
+# print( False ^ True )
+# print( False ^ False )
+
+    # BREAKOUT slide 36
+
+# control flow
+    # if
+    # elif
+    # else
+
+
+
+# keyword condition:
+    # body
+# if a > b:
+#     print("hello")
+# elif b > a:
+#     print("hi")
+# else:
+#     print("hey")
+
+# a = 5
+# b = 5
+# if a != b:
+#     print("yo")
+# if a >= b:
+#     print("something")
