0e77ef74e7155092ee014e7672496039aa1f56ff
Andrew Roberts <clownfragment@gmail.com>
2020-07-23 19:31:13 -0600

    update

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -1,1 +1,13 @@
-print('hello world')
+def mean(lst):
+    return sum(lst) / len(lst)
+
+# # using the accumulator pattern
+# def mean(lst):
+#     accum = 0
+#     for num in lst:
+#         accum += num
+
+#     return accum / len(lst)
+
+lst1 = [1,2,3,4,5,6,7,8,9]
+print(mean(lst1))
