7e9805ddf3e9f035242fae7dee4975ee85021bbe
Tovio Roberts <clownfragment@gmail.com>
2020-07-30 20:03:34 -0600

    u

--- stats/02_sets/sets.py
+++ stats/02_sets/sets.py
@@ -52,7 +52,38 @@ def union_mult_sets(*args):
                 set_union.append(item)
     return set_union
 
-print(union_mult_sets(list1, list2, list3))
+# print(union_mult_sets(list1, list2, list3))
 
+
+''' Slide 8 Breakout '''
+
+four_sided_die = [1,2,3,4]
+coin_flip = ['H', 'T']
+
+samp_space = []
+
+for roll in four_sided_die:
+    for flip1 in coin_flip:
+        for flip2 in coin_flip:
+            samp_space.append([roll, flip1, flip2])
+
+# for outcome in samp_space:
+#     print(outcome)
+
+A = [] # die roll shows exactly one pip
+for outcome in samp_space:
+    if outcome[0] == 1:
+        A.append(outcome)
+
+# print(A)
+
+B = [] # at least one coin flip results in heads
+for outcome in samp_space:
+    if outcome.count('H') >= 1:
+        B.append(outcome)
+
+# print(B)
+
+print(union(A, B))
 
 
