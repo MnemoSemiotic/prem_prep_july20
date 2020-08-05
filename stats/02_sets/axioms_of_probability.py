b76040d3717fb3b441626d82eda42cd7e98249a0
Tovio Roberts <clownfragment@gmail.com>
2020-08-04 19:56:56 -0600

    update

--- stats/02_sets/axioms_of_probability.py
+++ stats/02_sets/axioms_of_probability.py
@@ -40,6 +40,9 @@ c = True
 A ∪ (BC) = (A ∪ B)(A ∪ C) 
 A(B ∪ C) = (AB) ∪ (AC)
 '''
+a = True
+b = False
+c = True
 # print(setA.union(setB.intersection(setC)) == (setA.union(setB)).intersection(setA.union(setC)))
 
 # print((a or (b and c)) == (a and b) or (a and c))
@@ -50,6 +53,9 @@ Idempotent Laws
 A ∪ A = A
 AA = A
 '''
+a = True
+b = False
+c = True
 # print(setA.union(setA) == setA)
 # print((a and a) == a)
 
@@ -63,11 +69,54 @@ The set of which all other subsets are a subset of
 A ∩ U = A
 A ∩ ∅ = ∅
 '''
+a = True
+b = False
+c = True
 # print(setA.intersection(sample_space) == setA)
 
 U = {True, False}
 A = {True}
 null_set = set()
 
-print(A.intersection(U) == A)
-print(A.intersection(null_set) == null_set)
+# print(A.intersection(U) == A)
+# print(A.intersection(null_set) == null_set)
+
+
+'''
+Absorption Laws
+A ∪ (AB) = A
+A(A ∪ B) = A
+'''
+# print(setA.union(setA.intersection(setB)) == setA)
+# print(setA.intersection(setA.union(setB)) == setA)
+
+# print(a or (a and b) == a)
+# print(a and (a or b) == a)
+
+
+'''
+Identity Property
+A ∪ ∅ = A
+'''
+# print(setA.union(null_set) == setA)
+
+'''
+Other axioms of set theory
+Involution Law
+(A^c)^c = A
+
+we don't know the name of this
+AB ∪ AB^c = A
+'''
+# print(not (not a) == a)
+# print(((a and b) or (a and not b)) == a)
+
+
+'''
+DeMorgan’s Laws
+1st: (A ∪ B)^c = A^cB^c
+
+2nd: (AB)^c = A^c ∪ B^c
+'''
+# print((not (a or b)) == (not a) and (not b))
+# print(not (a and b) == (not a) or (not b))
