67206cfc5b910c603f9b4db7e3f55de46c75abe0
Tovio Roberts <clownfragment@gmail.com>
2020-08-06 20:43:58 -0600

    update

--- stats/05_discrete_distributions/binomial/binomial_using_pmf.py
+++ stats/05_discrete_distributions/binomial/binomial_using_pmf.py
@@ -8,3 +8,32 @@ def factorial(n):
 def combinations(n, k):
     return int(factorial(n) / (factorial(n-k) * factorial(k)))
 
+def binomial_pmf(n, k, p=0.5):
+    return combinations(n, k) * (p**k) * ((1-p)**(n-k))
+
+# print(binomial_pmf(8, 4, p=0.25))
+
+
+def binomial_cdf(n, k_high, p=0.5):
+    cumulative = 0
+
+    for k in range(k_high+1):
+        cumulative += binomial_pmf(n, k, p)
+    
+    return cumulative
+
+# print(binomial_cdf(n=8, k=4, p=0.25))
+
+def binomial_dict(n, k_low, k_high, p=0.5):
+    d = dict()
+
+    for k in range(k_low, k_high+1):
+        d[k] = binomial_pmf(n, k, p=p) 
+
+    return d
+
+# for k, v in binomial_dict(8, 0,  8, 0.25).items():
+#     print(f'{k}: {v}')
+
+
+
