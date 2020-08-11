39ab49e51571fd4dea467fddab6da8e0059eadf1
Tovio Roberts <clownfragment@gmail.com>
2020-08-10 20:01:53 -0600

    update

--- stats/05_discrete_distributions/poisson/poisson_using_pmf.py
+++ stats/05_discrete_distributions/poisson/poisson_using_pmf.py
@@ -9,4 +9,19 @@ def factorial(n):
     
     return prod
 
-print(factorial(5))
+# print(factorial(5))
+
+from math import e
+def poisson_pmf(lmbda, k):
+    return (lmbda**k * e**(-lmbda)) / factorial(k)
+
+# print(poisson_pmf(10, 10))
+
+
+'''
+One average, 7 birds fly across your field of vision every 10 minutes when you're observing in a protected wetland. What is the probability of observing 12 birds in 20 minutes?
+'''
+
+lmbda = 14 # 7 birds in 10 minutes
+k = 12
+print(poisson_pmf(lmbda, k))
