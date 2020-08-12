9845ea940b7aa63f715516ef060f986b7389d7fd
Tovio Roberts <clownfragment@gmail.com>
2020-08-11 20:43:43 -0600

    update

--- stats/05_discrete_distributions/geometric/geometric_sampling.py
+++ stats/05_discrete_distributions/geometric/geometric_sampling.py
@@ -19,3 +19,51 @@ def perform_geometric(p=0.5):
 
 # perform_geometric(p=0.05)
 
+
+def geometric(p=0.5):
+    # number of failures before the first success
+    lst = []
+
+    for _ in range(100000000):
+        trial = bernoulli(p)
+        lst.append(trial)
+
+        if trial == 1:
+            break
+
+    return len(lst) - 1
+
+# print(geometric(p=0.05))
+
+
+def geometric_samples_dict(p=0.05, num_samples=10000):
+    d = dict()
+
+    for _ in range(num_samples):
+        num_trials = geometric(p)
+
+        if num_trials not in d:
+            d[num_trials] = 0
+        d[num_trials] += 1
+    
+    return d
+
+# d = geometric_samples_dict(p=0.05, num_samples=10000)
+# for k, v in sorted(d.items()):
+#     print(f'{k}: {v}')
+
+
+def geometric_samples_proba_dict(p=0.05, num_samples=10000):
+    d = geometric_samples_dict(p=p, num_samples=num_samples)
+
+    new_d = dict()
+
+    for k, v in d.items():
+        new_d[k] = v / num_samples
+    
+    return new_d
+
+d = geometric_samples_proba_dict(p=0.5, num_samples=100000)
+
+for k, v in sorted(d.items()):
+    print(f'{k}: {v}')
