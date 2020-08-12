6ce68dc436ad146ed19dffa1c6a6d8dfce2b1211
Tovio Roberts <clownfragment@gmail.com>
2020-08-11 19:46:17 -0600

    update

--- stats/05_discrete_distributions/geometric/geometric_using_pmf.py
+++ stats/05_discrete_distributions/geometric/geometric_using_pmf.py
@@ -14,14 +14,30 @@ k=5
 
 
 def geometric_pmf(p, k, inclusive=True):
-    if inclusive:
+    if inclusive: # k is the trial with the first success
         return p * (1-p)**(k-1)
-    else:
+    else: # k is the number of failures
         return p * (1-p)**k
 
+'''
+You are flipping a fair coin. What is the probability that you get your first heads on the 7th flip?
+'''
+# print(geometric_pmf(0.5, 7, inclusive=True))   # 0.0078125
+# print(geometric_pmf(0.5, 6, inclusive=False))  # 0.0078125
+
 
 '''
-You are flipping a fair coin. What is the probability that you get a heads on the 7th flip?
+A roofer hits their thumb with a hammer 1/1000 times when they swing the hammer. What is the probability that the roofer will hit first hit their thumb after swinging the hammer 37 times (hits on the 38th)?
 '''
+# print(geometric_pmf((1/1000), 38, inclusive=True))  # 0.0009637
+# print(geometric_pmf((1/1000), 37, inclusive=False)) # 0.0009637
 
 
+'''
+Geometric cdf
+'''
+def geom_cdf_closed(p, k, inclusive=True):
+    if inclusive:
+        return 1 - (1-p)**k
+    else:
+        return 1 - (1-p)**(k+1)
