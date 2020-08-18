cbb30e78b42a2d51d3c1f419baf612c11f4b9284
Tovio Roberts <clownfragment@gmail.com>
2020-08-17 20:51:07 -0600

    add continuous files

--- stats/06_continuous_distributions/exponential.py
+++ stats/06_continuous_distributions/exponential.py
@@ -16,3 +16,23 @@ def exponential_variance(lmbda):
 
 def exponential_std(lmbda):
     return (1 / lmbda**2)**(1/2)
+
+'''
+Exponential Breakout 1
+Suppose you’re on a street corner trying to hail a taxi cab. Let 
+
+X = The amount of time (in minutes) that you have to wait. 
+
+X ~ exponential(0.1)
+
+Note: ƛ = 0.1 (meaning the rate of cab arrivals is 0.1 per minute)
+'''
+
+# What’s the probability that you’ll have to wait more than ten minutes? 
+print(1 - exponential_cdf(0.1, 10)) # 0.3679
+
+# What is the amount of time that you would expect to wait? 
+print(exponential_mean(0.1))
+
+# What is the variance of this random variable?
+print(exponential_variance(0.1))
---
+++ stats/06_continuous_distributions/normal.py
@@ -0,0 +1,19 @@
+from math import e, pi
+
+def normal_pdf(x, mu, sigma):
+    return (1 / (sigma * (2*pi)**(1/2))) * e**((-(1/2)*((x-mu)**2 / sigma**2)))
+
+def normal_cdf(x, mu, sigma):
+    vals = [num*0.001 for num in range(-10000, int(x*1000)+1)]
+
+    accum = 0
+    for val in vals:
+        res = normal_pdf(val, mu, sigma)
+        accum += res
+
+        if val > x:
+            break
+    return accum*0.001
+
+
+print(1 - normal_cdf(100, 90, 10))
